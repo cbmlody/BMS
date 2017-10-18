da7e0da0b8d82e2ed2f71a8e8a6f93763f0beb52
Cezary Broś <cezary.bros+git@gmail.com>
2017-10-18 17:30:49 +0200

    Remove unused imports

--- BloodManagmentSystem/App_Start/IdentityConfig.cs
+++ BloodManagmentSystem/App_Start/IdentityConfig.cs
@@ -9,8 +9,6 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using RestSharp;
-using RestSharp.Authenticators;
 
 namespace BloodManagmentSystem
 {
